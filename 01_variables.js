9ceb6f5d7de23ac02a15f59b6bbd0ac19101a4f7
Asti-exe <astitvaa22@gmail.com>
2025-10-15 17:18:47 +0530

    Variable and datatypes

---
+++ 01_variables.js
@@ -0,0 +1,22 @@
+ const accountId = 907675;
+let accountEmail = "astitvaa22@gmail.com";
+var accountPassword = "Astitva@22";
+accountCity = "Delhi";
+let accountState;
+
+// accountId = 2; // we cannot change a constant variable
+
+// accountEmail = "hello@gmail.com";
+// accountPassword = "12345";
+// accountCity = "Noida";
+
+
+/*
+Prefer not to use var
+because of block scope and functional scope issues
+*/
+
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// Treat all JS code as newer JS code
+
+// alert("2 +3"); // we cannot use alert in node js environment. We are using node.js not browser
+console.log(2+3); // we can use console.log in node js environment
+
+// Code Readibility should be high
+
+// tc39 ecma script and MDN docs.
+
+console.log("Hello World"); // we can use console.log in node js environment
+
+// number => 2 to the power of 53
+// bigint => larger than 2 to the power of 53
+// string=> "",'',``
+// boolean => true/false
+// null => standalone value
+// undefined => variable value not assigned
+// symbol => unique
+
+
+// object => key value pairs
+// array => ordered list of values
+
+console.log(typeof "Astitva", 5); // infinity
